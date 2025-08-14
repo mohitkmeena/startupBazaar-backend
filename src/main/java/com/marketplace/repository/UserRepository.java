93ab93c35132cbb4759907ee9e81a9b05fecfa81
mohitkmeena <mohitmeenaG2005@gmail.com>
2025-08-14 09:44:43 +0530

    user repo created

---
+++ src/main/java/com/marketplace/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.marketplace.repository;
+
+import com.marketplace.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserId(String userId);
+    boolean existsByEmail(String email);
+}
